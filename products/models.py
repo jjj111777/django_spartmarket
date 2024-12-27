3b6f30c517b39f6b4f6855aeba7af706d9b656e9
suk_hoon <jjj11177742@gmail.com>
2024-12-27 14:08:56 +0900

    찜기능

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    # 찜하기 모델 추가
+    class Product(models.Model):
+    # 기존 필드들...
+    liked_by = models.ManyToManyField(User, related_name='liked_products', blank=True)
+
+
+# 찜하기 뷰 추가
+def like_product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    product.liked_by.add(request.user)  # 로그인한 유저를 찜 목록에 추가
+    return redirect('product_detail', product_id=product.id)
+
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
+from .forms import ProductForm
 
-# Create your views here.
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            form.instance.user = request.user  # 현재 로그인한 유저로 설정
+            form.save()
+            return redirect('home')
+    else:
+        form = ProductForm()
+    return render(request, 'products/product_form.html', {'form': form})
