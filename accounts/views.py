7b32327cdd608b3d867d962cad9765baf3904284
suk_hoon <jjj11177742@gmail.com>
2024-12-27 13:51:35 +0900

    로그인

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
+#회원가입입
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -13,3 +14,15 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
+#로그인
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # 로그인 후 홈으로 이동
+        else:
+            messages.error(request, '아이디 또는 비밀번호가 잘못되었습니다.')
+    return render(request, 'accounts/login.html')
--- spartmarket/urls.py
+++ spartmarket/urls.py
@@ -20,4 +20,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', views.signup, name='signup'),
+    path('login/', views.user_login, name='login'),
 ]
